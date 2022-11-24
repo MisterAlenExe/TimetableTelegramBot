54cc968b880a4de8ab27ad5bf0c729249bb1e7f9
misteralen <alen.sultanov@gmail.com>
2022-11-24 21:58:27 +0600

    Refactor code, add logger system

--- bot.py
+++ bot.py
@@ -1,82 +1,30 @@
 import asyncio
-import logging
-import warnings
 
-from pytz_deprecation_shim import PytzUsageWarning
-from aiogram import Bot, Dispatcher
+from aiogram import Bot
+from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.contrib.fsm_storage.redis import RedisStorage2
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+from tgbot.logger import logger
 from tgbot.config import load_config
-from tgbot.middlewares.environment import EnvironmentMiddleware
-from tgbot.middlewares.scheduler import SchedulerMiddleware
-from tgbot.filters.admin import IsAdmin
-from tgbot.handlers.admin import register_admin
-from tgbot.handlers.user import register_user
-from tgbot.handlers.menu import register_menu, register_callback_menu
-from tgbot.handlers.notify import register_notify_cmd, daily_notify
 
-logger = logging.getLogger(__name__)
-
-
-def register_all_middlewares(dp, config):
-    dp.setup_middleware(EnvironmentMiddleware(config=config))
-    dp.setup_middleware(SchedulerMiddleware(config))
-
-
-def register_all_filters(dp):
-    dp.filters_factory.bind(IsAdmin)
-
-
-def register_all_handlers(dp):
-    register_admin(dp)
-    register_user(dp)
-    register_menu(dp)
-    register_callback_menu(dp)
-    register_notify_cmd(dp)
-
-
-def set_scheduled_jobs(scheduler, bot, config):
-    scheduler.add_job(daily_notify, 'cron', day_of_week='mon-sat', hour=7, minute=0,
-                      args=(bot,))
+from tgbot.handlers.menu import register_menu
 
 
 async def main():
-    logging.basicConfig(
-        level=logging.INFO,
-        format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
-    )
+    data_config = load_config()
+
     logger.info("Starting bot")
-    config = load_config(".env")
 
-    storage = RedisStorage2() if config.tg_bot.use_redis else MemoryStorage()
-    bot = Bot(token=config.tg_bot.token, parse_mode='HTML')
-    dp = Dispatcher(bot, storage=storage)
+    bot = Bot(token=data_config['bot_token'])
+    dp = Dispatcher(bot, storage=MemoryStorage())
 
-    warnings.filterwarnings(action="ignore", category=PytzUsageWarning)
-    scheduler = AsyncIOScheduler()
+    register_menu(dp)
 
-    bot['config'] = config
-
-    register_all_middlewares(dp, config)
-    register_all_filters(dp)
-    register_all_handlers(dp)
-
-    set_scheduled_jobs(scheduler, bot, config)
-
-    # start
-    try:
-        scheduler.start()
-        await dp.start_polling()
-    finally:
-        await dp.storage.close()
-        await dp.storage.wait_closed()
-        await bot.session.close()
+    await dp.start_polling()
 
 
 if __name__ == '__main__':
     try:
         asyncio.run(main())
     except (KeyboardInterrupt, SystemExit):
-        logger.error("Bot stopped!")
+        logger.error("Bot stopped")
